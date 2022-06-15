4a47fb2eb99e98144b6f58c600eb618f3a2951a8
Tumon <tumon@debian.Tumon>
2022-06-15 16:27:45 +0600

    init

--- models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String
-})
-
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
