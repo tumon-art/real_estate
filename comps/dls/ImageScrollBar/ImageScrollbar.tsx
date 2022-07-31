50ac31ae3b16cef0f554f19259da616b9e829389
tumon <tumonkhn@gmail.com>
2022-08-01 00:39:05 +0600

    init

--- comps/dls/ImageScrollBar/ImageScrollbar.tsx
+++ comps/dls/ImageScrollBar/ImageScrollbar.tsx
@@ -1,59 +1,59 @@
-import { useState } from "react";
-import styles from './ImageS.module.scss'
+import styles from "./ImageS.module.scss";
 
 const ImageScrollbar = ({ photos }: any) => {
-
-  const [index,setindex] = useState(0)
   const rightArrow = () => {
-    const auto = document.getElementById('auto') as HTMLElement
-    auto.scrollLeft= auto?.scrollLeft + (1400/4)
-    console.log(auto?.scrollBy)
+    const auto = document.getElementById("auto") as HTMLElement;
+    auto.scrollLeft = auto?.scrollLeft + 1400 / 4;
+    console.log(auto?.scrollBy);
   };
 
   const leftArrow = () => {
-    const auto = document.getElementById('auto') as HTMLElement
-    auto.scrollLeft= auto?.scrollLeft - (1400/4)
-    console.log(auto?.scrollBy)
+    const auto = document.getElementById("auto") as HTMLElement;
+    auto.scrollLeft = auto?.scrollLeft - 1400 / 4;
+    console.log(auto?.scrollBy);
   };
 
   return (
-
     <div className=" relative">
-
       {/* === LEFT ARROW */}
-      <span onClick={()=> leftArrow()}
-      className={`${styles.text} absolute text-6xl font-bold
+      <span
+        onClick={() => leftArrow()}
+        className={`${styles.text}  hidden sm:flex absolute text-6xl font-bold
       hover:text-sky-200 cursor-pointer  text-white  shadow-2xl
-     mt-auto mb-auto flex items-center left-2 md:left-10 top-0 bottom-0 `}>
+     mt-auto mb-auto flex items-center left-2 md:left-10 top-0 bottom-0 `}
+      >
         ❰
       </span>
 
       {/* === RIGHT ARROW */}
-      <span onClick={()=>rightArrow()}
-       className={`${styles.text}  absolute text-6xl font-bold
+      <span
+        onClick={() => rightArrow()}
+        className={`${styles.text} hidden sm:flex  absolute text-6xl font-bold
        hover:text-sky-200 cursor-pointer  text-white shadow-2xl
-       mt-auto mb-auto flex items-center right-2 md:right-10 top-0 bottom-0 `}>
+       mt-auto mb-auto flex items-center right-2 md:right-10 top-0 bottom-0 `}
+      >
         ❱
       </span>
 
-    <div id='auto'
-    className=" ring-8 ring-sky-100 my-4 md:rounded-xl overflow-auto scroll-smooth h-[333px] flex">
-      {photos.slice(0,4).map((e: any, i: any) => {
-        return (
-              <img id={i}
-              loading='lazy'
+      <div
+        id="auto"
+        className=" scroll sm:ring-8 ring-sky-100 my-4 md:rounded-xl overflow-auto scroll-smooth h-[333px] flex"
+      >
+        {photos.slice(0, 4).map((e: any, i: any) => {
+          return (
+            <img
+              id={i}
+              loading="lazy"
               className="cursor-grab object-cover"
               key={i}
-              width='1400'
-              height='200'
+              width="1400"
+              height="200"
               src={e.url}
               alt="img"
             />
-
-        );
-      })}
-      
-    </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,10 +1,5 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import { BsFilter } from "react-icons/bs";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
-import { GoVerified } from "react-icons/go";
 import millify from "millify";
 import { baseUrl, fetchApi } from "../../utils/fetchData";
 import ImageScrollbar from "../../comps/dls/ImageScrollBar/ImageScrollbar";
@@ -18,12 +13,7 @@ const Property = ({
     baths,
     area,
     agency,
-    isVerified,
     description,
-    type,
-    purpose,
-    furnishingStatus,
-    amenities,
     photos,
   },
 }: any) => {
@@ -31,26 +21,25 @@ const Property = ({
     <div className=" font-FiraMono md:px-20 text-sky-700">
       {photos && <ImageScrollbar photos={photos} />}
 
-   <div className=" mx-2 sm:mx-0 flex justify-between">
-   <div className="  font-bold">
-        AED
-        <span className="   ml-2 text-sky-800 ">{millify(price)}</span>{" "}
-        {rentFrequency && ` /${rentFrequency}`}
-        
-      </div>
+      <div className=" mx-2 sm:mx-0 flex justify-between">
+        <div className="  font-bold">
+          AED
+          <span className="   ml-2 text-sky-800 ">{millify(price)}</span>{" "}
+          {rentFrequency && ` /${rentFrequency}`}
+        </div>
 
-      <div className="ring-4  rounded-full flex justify-center items-center">
-              {/* === AVATAR */}
-              <img
-                className=" h-6 w-6 object-cover rounded-full"
-                src={agency?.logo?.url}
-                width="30"
-                height="30"
-                alt="avater"
-                loading="lazy"
-              />
-            </div>
-   </div>
+        <div className="ring-4  rounded-full flex justify-center items-center">
+          {/* === AVATAR */}
+          <img
+            className=" h-6 w-6 object-cover rounded-full"
+            src={agency?.logo?.url}
+            width="30"
+            height="30"
+            alt="avater"
+            loading="lazy"
+          />
+        </div>
+      </div>
 
       <div className=" mx-2 sm:mx-0 my-2">
         <div className=" flex gap-3 items-center text-cyan-700">
@@ -60,11 +49,11 @@ const Property = ({
 
         <h1 className=" my-2 text-lg sm:text-xl font-bold"> {title}</h1>
         <hr className=" mt-5"></hr>
-        <p className="  my-10 text-zinc-900 whitespace-pre-line"> {description} </p>
-
+        <p className="  my-10 text-zinc-900 whitespace-pre-line">
+          {" "}
+          {description}{" "}
+        </p>
       </div>
-
-      
     </div>
   );
 };
