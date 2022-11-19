5ab9875912cf98257144acfc73e6c8fc565fb52c
tumon <tumonkhn@gmail.com>
2022-11-19 12:09:04 +0600

    init

--- comps/Tour.tsx
+++ comps/Tour.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { DaysTypes } from "../pages/property/[id]";
 import QuestionMark from "./svg/QuestionMark";
 
@@ -20,8 +21,24 @@ available options with the agent you are connected with.`
 
 export default function Tour({ days }: { days: DaysTypes[] }) {
 
+  useEffect(() => {
+    let marker = document.getElementById('marker');
+    let list: HTMLLIElement[] | any = document.querySelectorAll('ol li');
+    marker!.style.left = list[0].offsetLeft + 'px';
+    marker!.style.width = list[0].offsetWidth + 'px';
+
+    console.log(list[0])
+    function moveIndicator(e: HTMLLIElement) {
+      marker!.style.left = e.offsetLeft + 'px';
+      marker!.style.width = e.offsetWidth + 'px';
+    }
+    list.forEach((each: HTMLLIElement) => {
+      each.addEventListener('click', (e: HTMLLIElement | any) => moveIndicator(e.target))
+    })
+  }, [])
+
   return (
-    <div>
+    <div className="md:w-2/4 w-full">
       <h3 className=" font-bold ">Schedule A Tour</h3>
       <div className=" flex gap-2 items-center text-gray-400">
         <span> Tour Type</span>
@@ -29,14 +46,16 @@ export default function Tour({ days }: { days: DaysTypes[] }) {
           text={textProp}
         />
       </div>
-      <div className=" bg-gray-500">
-        <div>
+      <ol className=" my-4 relative w-full text-zinc-600
+      bg-gray-200 py-2 font-bold isolate ">
+        <li className=" px-5 w-1/2 text-center inline-block cursor-pointer">
           In-Person
-        </div>
-        <div>
+        </li>
+        <li className=" px-5 w-1/2 text-center inline-block cursor-pointer">
           Video Chat
-        </div>
-      </div>
+        </li>
+        <div id='marker' className="left-0 transition-all duration-1000 z-[-1] absolute top-0 h-full w-72 bg-sky-200 "></div>
+      </ol>
     </div>
   )
 }
