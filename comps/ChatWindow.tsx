f38977084e1bda1557fe1aee140dc39c13c99ff7
tumon <tumonkhn@gmail.com>
2023-01-29 01:28:19 +0600

    init

--- comps/ChatWindow.tsx
+++ comps/ChatWindow.tsx
@@ -2,12 +2,17 @@ export default function ChatWindow({ setShowWindow }: { setShowWindow: any }) {
 
   return (
     <nav className=" fixed h-[100vh] w-[100vw] z-50">
+
       <div className="fixed h-[100vh] w-[100vw]"
         onClick={() => setShowWindow(false)}
       > </div>
-      <nav className="right-1 sm:right-5 bottom-20 fixed
-    shadow-2xl ring-4 ring-sky-200
-    h-96 w-80 z-20 rounded-xl backdrop-blur-md"> </nav>
+
+      <nav className="right-1 flex justify-center sm:right-5 bottom-20 fixed
+    shadow-2xl ring-4 items-center ring-sky-200 overflow-hidden
+    h-96 w-60 sm:w-80 z-20 rounded-xl backdrop-blur-md">
+    <div className="absolute clipP w-full  h-full bg-sky-400  opacity-60"> </div>
+    hello
+    </nav>
 
     </nav>
   )
