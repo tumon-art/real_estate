6fa0d437b11d965229fa05466c37e37dfcd2739d
tumon <tumonkhn@gmail.com>
2023-02-18 01:52:55 +0600

    .

--- comps/dls/Banner/Banner.tsx
+++ comps/dls/Banner/Banner.tsx
@@ -3,17 +3,15 @@ import Image from "next/image";
 import houseImg from "../../../assets/images/real3.webp";
 
 const Banner = () => (
-  <div className=" w-full">
-    <Image
-      src={houseImg}
-      alt="img"
-      height="400"
-      width="1200"
-      className="object-cover overflow-hidden
+  <Image
+    src="/real3.webp"
+    alt="img"
+    height="500"
+    width="1200"
+    className="object-cover overflow-hidden
        w-full h-[300px] sm:h-[400px] gap-5"
-      priority
-    />
-  </div>
+    priority
+  />
 );
 
 export default Banner;
