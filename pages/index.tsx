a55bf6872b81d40c1e52d9c27cfb7d193854beab
tumon <tumonkhn@gmail.com>
2023-02-19 14:51:56 +0600

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-// TODO fix chat
 import { useEffect, useState } from "react";
 import { MdSell } from "react-icons/md";
 import Agents from "../comps/Agents";
@@ -38,9 +37,9 @@ export default function Home({ propertyForSale, propertyForRent }: any) {
 
   const ForWhat = ({ text }: { text: string }) => (
     <h2
-      className=" inline-block self-start mx-1 sm:mx-10 text-xl font-extrabold
-  text-sky-500 border-b-4 ring-sky-400 border-sky-400 my-1
-   sm:my-5 sm:ring-2 px-4 rounded-sm"
+      className=" block  sm:inline-block self-start mx-1 sm:mx-10 text-xl font-extrabold
+      text-sky-500 border-t-2 border-sky-300 sm:border-t-0 sm:border-b-4 ring-sky-400
+      sm:border-sky-400 my-1 sm:my-5 sm:ring-2 px-4 rounded-xl sm:rounded-sm"
     >
       <span className="flex items-center gap-2">
         <MdSell /> {text}
